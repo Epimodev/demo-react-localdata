4cd2f127244e2faf0f8c68e641c141d79f4d6cd6
Frederic <frederic@pandascore.co>
2019-02-02 14:50:35 +0100

    Init project

---
+++ src/Application/index.tsx
@@ -0,0 +1,14 @@
+import { createElement } from 'react';
+import Page1 from 'src/pages/Page1';
+import 'assets/fonts/fonts.css';
+import './style.scss';
+
+function Application() {
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+}
+
+export default Application;
